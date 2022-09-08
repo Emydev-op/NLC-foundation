770108468618fd31b5946b598db231c2202629e8
Chukwuemeka Osegbo <alphaoseghe@gmail.com>
2022-09-08 16:14:38 +0100

    Added the assignment

---
+++ C_intro/getreturnvalue.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * @brief Assignment on C
+ * 
+ */
+int add_two(int x, int y) {
+  return x + y;
+}
+
+int main() {
+  printf("Result is: %d\n", add_two(5, 3));
+
+  return 0;
+}
---
+++ C_intro/practice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int main()
+{
+    // char
+    char C = '1';
+    // Print 'char is 1 byte'
+
+    printf("char is %c byte \n", C);
+    return 0;
+}
