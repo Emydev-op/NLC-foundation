cbc3a42355be1ed621b21b1547e4706b07c5a731
Chukwuemeka Osegbo <alphaoseghe@gmail.com>
2022-09-12 11:45:52 +0100

    Finished the assignment

--- C_intro/limit.c
+++ C_intro/limit.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 #include <limits.h>
+#include <float.h>
 
+int main()
+{
+    printf("%d\n", INT_MAX);
+    printf("%d\n", INT_MIN);
+    printf("%c\n", CHAR_MAX);
+    printf("%c\n",CHAR_MIN);
+    printf("%f\n", FLT_MAX);
+    printf("%f\n", FLT_MIN);
+    printf("%ld\n", LONG_MAX);
+    printf("%ld\n", LONG_MIN);
+    printf("%hu\n", SHRT_MAX);
+    printf("%hu\n", SHRT_MIN);
+    printf("%lf\n", DBL_MAX);
+    printf("%lf\n", DBL_MIN);
+    return 0;
+}
