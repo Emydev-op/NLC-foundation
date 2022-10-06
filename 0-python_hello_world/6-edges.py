10e98bad89bdd5aeb2d41fcab10784ad398c6f0a
Chukwuemeka Osegbo <alphaoseghe@gmail.com>
2022-10-06 20:26:09 +0100

    Finished. I think!

--- 0-python_hello_world/6-edges.py
+++ 0-python_hello_world/6-edges.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+word = "Nervelabs"
+first = word[0:3]
+mid= word[1:8]
+last = word[7:]
+print("First 3 letters: {}".format(first))
+print("Last 2 letters: {}".format(last))
+print("Middle Word: {}".format(mid))
--- 0-python_hello_world/7-concat_edges.py
+++ 0-python_hello_world/7-concat_edges.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+txt = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+con1 = txt[39:66]
+con2 = txt[106:112]
+con3 = txt[:6]
+print(con1 + con2 + con3)
